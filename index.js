652f48f327af23ae63b5dc4061e0bb9341463b1a
Mario-Ronzon <mario.ronzon5929@alumnos.udg.mx>
2022-02-23 21:01:57 -0600

    --

---
+++ index.js
@@ -0,0 +1,39 @@
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )//angulo, prop imagen, near lim, far lim
+const renderer = new THREE.WebGLRenderer({ antialias: true})
+
+renderer.setSize( window.innerWidth, window.innerHeight )
+document.body.appendChild( renderer.domElement )//<-----------------
+
+var geometry = new THREE.SphereGeometry( 1, 32, 16)//radio, subdivisiones ancho, sub alto (contiene informacion poligonal)
+var material = new THREE.MeshStandardMaterial( { color: 0x0dc4ff })//contiene informacion del material
+var cube = new THREE.Mesh ( geometry, material )//contiene la geometria y el material
+
+var geometry2 = new THREE.BoxGeometry( 2, 3, 2)//x,y,z
+var material2 = new THREE.MeshBasicMaterial( {
+ color: "#ffb1ef", wireframe: true, transparent: true})
+var wireframeCube = new THREE.Mesh ( geometry2, material2 )
+
+scene.add( wireframeCube )
+scene.add( cube )
+
+renderer.render( scene, camera )
+camera.position.z = 5
+
+var ambientLight = new THREE.AmbientLight ( 0xffffff, 0.5)
+scene.add( ambientLight )
+
+var pointLight = new THREE.PointLight( 0xffffff, 1 );
+pointLight.position.set( 25, 50, 25 );
+scene.add( pointLight );
+
+function animate() {
+    requestAnimationFrame( animate )
+    cube.rotation.x += 0.04;
+    cube.rotation.y += 0.00;
+    wireframeCube.rotation.x -= 0.01;
+    wireframeCube.rotation.y -= 0.1;
+    renderer.render( scene, camera )
+}
+
+animate()
